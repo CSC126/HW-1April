487f7047d7ba8492ff6a8e2b33f0f04a91649787
Alan Mackiewicz <alanmackiewicz@yahoo.com>
2022-04-30 14:53:43 -0400

    Added first function to code and tested it

--- HW#1April/main.cpp
+++ HW#1April/main.cpp
@@ -4,9 +4,22 @@
 #include <cmath>
 #include <ctime>
 #include <string>
+using namespace std;
+bool allSame(int, int);
 
 int main(){
-    
+
+    bool ans = allSame(2, 2);
+    cout << ans << endl;
     
     return 0;
+}
+
+bool allSame(int a, int b){
+    if(((a % 2 == 0)&&(b % 2 == 0)) || ((a % 2 == 1)&&(b % 2 == 1))){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
