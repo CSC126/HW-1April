64eab973c472b4a0603290b8e300dd8f6a0bbda1
Alan Mackiewicz <alanmackiewicz@yahoo.com>
2022-04-30 14:58:50 -0400

    added check Divisibility function

--- HW#1April/main.cpp
+++ HW#1April/main.cpp
@@ -6,11 +6,14 @@
 #include <string>
 using namespace std;
 bool allSame(int, int);
+bool checkDivisibility(int, int);
 
 int main(){
 
     bool ans = allSame(2, 2);
     cout << ans << endl;
+    bool ans2 = checkDivisibility(2, 4);
+    cout << ans2 << endl;
     
     return 0;
 }
@@ -22,4 +25,14 @@ bool allSame(int a, int b){
     else{
         return false;
     }
+}
+
+bool checkDivisibility(int a, int b){
+    if((a % b == 0) || (b % a == 0)){
+        return true;
+    }
+    else{
+        return false;
+    }
+    
 }
